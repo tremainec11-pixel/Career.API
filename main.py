8ceaedc747e31a17385a5b5f12c7076903ed52e1
Tremaine Nathan Charles <tremainecharles@hotmail.com>
2026-09-23 09:38:47 -0600

    Allow production frontend CORS

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://careerai-frontend-9tyr.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
